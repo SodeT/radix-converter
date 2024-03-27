e45f2bb04c1b642058543e472af129ec677283e1
SodeT <42680887+SodeT@users.noreply.github.com>
2024-03-27 12:10:19 +0100

    Fractions + working UI

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"strings"
 	"math"
+	"os"
+	"strconv"
+	"log"
 )
 
 var (
@@ -12,32 +15,100 @@ var (
 )
 
 func main() {
-	pl(decode("fff", 16))
-	pl(encode(23, 16))
-}
+	if len(os.Args) != 4 {
+		pl(
+`Usage: radix-convert <number> <from radix> <to radix>
 
-func decode(number string, radix int) int {
-	totalValue := 0
-	digits := []rune(number)
+Description:
+	This command converts a number from any radix to any other radix.
+		
+Arguments:
+	<number>            The number to be converted.
+	<from radix>        The current radix (base) of the number.
+	<to radix>          The target radix (base) to which the number will be converted.
 
-	for i, j := 0, len(digits)-1; i < j; i, j = i+1, j-1 {
-		digits[i], digits[j] = digits[j], digits[i]
+Note:
+	* This program only works for radices up to 64.
+	* For base64, the output does not comply with RFC 4648 (RFC 4648 ordering [A-Z][a-z][0-9]+/)
+	* Upper and lowercase letters are never considerd to be equal.
+		`)
+		return
 	}
 
-	for index, digit := range digits {
-		value := strings.IndexRune(chars, digit)
-		totalValue += int(math.Pow(float64(radix), float64(index))) * value
+	number := os.Args[1]
+	radix1, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatal("Invalid radix...")
 	}
-	return totalValue
+
+	radix2, _ := strconv.Atoi(os.Args[3])
+	if err != nil {
+		log.Fatal("Invalid radix...")
+	}
+
+	n := decode(number, radix1)
+	pl(encode(n, radix2))
 }
 
-func encode(number int, radix int) string {
+func decode(number string, radix int) float64 {
+	strs := strings.Split(number, ".")
+
+	totalInt := 0
+	runeInts := []rune(strs[0])
+	for i, j := 0, len(runeInts)-1; i < j; i, j = i+1, j-1 {
+		runeInts[i], runeInts[j] = runeInts[j], runeInts[i]
+	}
+
+	for index, runeInt := range runeInts {
+		value := strings.IndexRune(chars, runeInt)
+		if value == -1 || value >= radix {
+			log.Fatal("Number is not in specified radix...")
+		}
+
+		totalInt += int(math.Pow(float64(radix), float64(index))) * value
+	}
+
+	totalFraction := 0.0
+	if len(strs) == 2 {
+		runeFractions := []rune(strs[1]) 
+		for i, j := 0, len(runeFractions)-1; i < j; i, j = i+1, j-1 {
+			runeFractions[i], runeFractions[j] = runeFractions[j], runeFractions[i]
+		}
+
+		for index, runeFraction := range runeFractions {
+			value := strings.IndexRune(chars, runeFraction)
+			if value == -1 || value >= radix {
+				log.Fatal("Number is not in specified radix...")
+			}
+
+			totalFraction += math.Pow(float64(radix), -float64(index + 1)) * float64(value)
+		}
+	}
+
+	return float64(totalInt) + totalFraction
+}
+
+
+func encode(number float64, radix int) string {
 	digits := []rune{}
-	for number != 0 { 
-		rest := number % radix
+	num := int(number)
+	fractionalDigits := 0
+
+	for number - float64(num) > 0 {
+		number *= float64(radix)
+		num = int(number)
+		fractionalDigits++
+	}
+	 
+	for num != 0 { 
+		rest := num % radix
 		digits = append(digits, rune(chars[rest]))
-		number -= rest
-		number /= radix 
+		num -= rest
+		num /= radix
+
+		if len(digits) == fractionalDigits {
+			digits = append(digits, '.')
+		}
 	}
 
 	for i, j := 0, len(digits)-1; i < j; i, j = i+1, j-1 {
